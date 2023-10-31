be16889a0fd76ea045ce5bf3e9d188e08cf853a0
Daniel Mironov <mail@danielmironov.dev>
2023-10-31 23:03:14 +0100

    added init commands

--- gc9a01.cpp
+++ gc9a01.cpp
@@ -1,7 +1,109 @@
 #include <stdio.h>
 #include "gc9a01.h"
 
-void func(void)
-{
+#define CMD_ID                          0x04
+#define CMD_DISPLAY_STATUS              0x09
+#define CMD_SLEEP_ON                    0x10
+#define CMD_SLEEP_OFF                   0x11
+#define CMD_PARTIAL_MODE_ON             0x12
+#define CMD_NORMAL_MODE_ON              0x13
+#define CMD_INVERT_OFF                  0x20
+#define CMD_INVERT_ON                   0x21
+#define CMD_DISPLAY_OFF                 0x28
+#define CMD_DISPLAY_ON                  0x29
+//...
+#define CMD_MEMORY_WRITE                0x2C
+#define CMD_PARTIAL_AREA                0x30
+#define CMD_VERTICAL_SCROLL             0x33
+#define CMD_TEARING_OFF                 0x34
+#define CMD_TEARING_ON                  0x35
+#define CMD_MEM_ACCESS_CTL              0x36
+#define CMD_VERTICAL_SCROLL_START_ADDR  0x37
+#define CMD_IDLE_OFF                    0x38
+#define CMD_IDLE_ON                     0x39
+#define CMD_PIXEL_FORMAT_SET            0x3A
+#define CMD_WRITE_MEM_CONTINUE          0x3C
+#define CMD_SET_TEAR_SCANLINE           0x44
+#define CMD_GET_SCANLINE                0x45
+#define CMD_BRIGHTNESS                  0x51
+#define CMD_CTRL_DISPLAY                0x53
+#define CMD_READ_ID1                    0xDA
+#define CMD_READ_ID2                    0xDB
+#define CMD_READ_ID3                    0xDC
 
+// Extended command set as found in the datasheet
+#define CMD_RGB_INTERFACE_SIGNAL_CTRL   0xB0
+#define CMD_BLANKING_PORCH_CTRL         0xB5
+#define CMD_DISPLAY_FUNCTION_CTRL       0xB6
+#define CMD_TE_CTRL                     0xBA
+#define CMD_INTERFACE_CTRL              0xF6
+#define CMD_POWER_CRITERION_CTRL        0xC1
+#define CMD_VCORE_VOLTAGE_CTRL          0xA7
+#define CMD_VREG1A_VOLTAGE_CTRL         0xC3
+#define CMD_VREG1B_VOLTAGE_CTRL         0xC4
+#define CMD_VREG2A_VOLTAGE_CTRL         0xC9
+#define CMD_FRAMERATE                   0xE8
+#define CMD_SPI_2_DATA_CTRL             0xE9
+#define CMD_CHARGE_PUMP_FREQENT_CTRL    0xEC
+#define CMD_INNER_REG_EN_1              0xFE
+#define CMD_INNER_REG_EN_2              0xEF
+#define CMD_SET_GAMMA_1                 0xF0
+#define CMD_SET_GAMMA_2                 0xF1
+#define CMD_SET_GAMMA_3                 0xF2
+#define CMD_SET_GAMMA_4                 0xF3
+
+
+static const gc9a01_cmd_t gc9a01_init_cmds[] {
+    {CMD_INNER_REG_EN_2, {0}, 0},
+    {0xeb, {0x14}, 1},
+    {CMD_INNER_REG_EN_1, {0}, 0},
+    {CMD_INNER_REG_EN_2, {0}, 0},
+    {0xeb, {0x14}, 1},
+    {0x84, {0x40}, 1},
+    {0x85, {0xff}, 1},
+    {0x86, {0xff}, 1},
+    {0x87, {0xff}, 1},
+    {0x88, {0x0a}, 1},
+    {0x89, {0x21}, 1},
+    {0x8a, {0x00}, 1},
+    {0x8b, {0x80}, 1},
+    {0x8c, {0x01}, 1},
+    {0x8d, {0x01}, 1},
+    {0x8e, {0xff}, 1},
+    {0x8f, {0xff}, 1},
+    {CMD_DISPLAY_FUNCTION_CTRL, {0x00,0x20}, 2},  // Scan direction S360 -> S1
+    // {CMD_MEM_ACCESS_CTL,{0x08},1},           //MemAccessModeSet(0, 0, 0, 1);
+    // {CMD_PIXEL_FORMAT_SET,{ColorMode_MCU_16bit&0x77},1},
+    {0x90, {0x08,0x08,0x08,0x08}, 4},
+    {0xbd, {0x06}, 1},
+    {0xbc, {0x00}, 1},
+    {0xff, {0x60,0x01,0x04}, 3},
+    {CMD_VREG1A_VOLTAGE_CTRL, {0x13}, 1},
+    {CMD_VREG1B_VOLTAGE_CTRL, {0x13}, 1},
+    {CMD_VREG2A_VOLTAGE_CTRL, {0x22}, 1},
+    {0xbe, {0x11}, 1},
+    {0xe1, {0x10,0x0e}, 2},
+    {0xdf, {0x21,0x0c,0x02}, 3},
+    {CMD_SET_GAMMA_1, {0x45,0x09,0x08,0x08,0x26,0x2a}, 6},
+    {CMD_SET_GAMMA_2, {0x43,0x70,0x72,0x36,0x37,0x6f}, 6},
+    {CMD_SET_GAMMA_3, {0x45,0x09,0x08,0x08,0x26,0x2a}, 6},
+    {CMD_SET_GAMMA_4, {0x43,0x70,0x72,0x36,0x37,0x6f}, 6},
+    {0xed, {0x1b,0x0b}, 2},
+    {0xae, {0x77}, 1},
+    {0xcd, {0x63}, 1},
+    {0x70, {0x07,0x07,0x04,0x0e,0x0f,0x09,0x07,0x08,0x03}, 9},
+    {CMD_FRAMERATE, {0x34}, 1},// 4 dot inversion
+    {0x62, {0x18,0x0D,0x71,0xED,0x70,0x70,0x18,0x0F,0x71,0xEF,0x70,0x70}, 12},
+    {0x63, {0x18,0x11,0x71,0xF1,0x70,0x70,0x18,0x13,0x71,0xF3,0x70,0x70}, 12},
+    {0x64, {0x28,0x29,0xF1,0x01,0xF1,0x00,0x07}, 7},
+    {0x66, {0x3C,0x00,0xCD,0x67,0x45,0x45,0x10,0x00,0x00,0x00}, 10},
+    {0x67, {0x00,0x3C,0x00,0x00,0x00,0x01,0x54,0x10,0x32,0x98}, 10},
+    {0x74, {0x10,0x85,0x80,0x00,0x00,0x4E,0x00}, 7},
+    {0x98, {0x3e,0x07}, 2},
+    {CMD_TEARING_ON, {0}, 0},// Tearing effect line on
+    {0, {0}, 0xff},//END
+};
+
+GC9A01::GC9A01() {
+    
 }
--- include/gc9a01.h
+++ include/gc9a01.h
@@ -1,1 +1,52 @@
-void func(void);
+#include <cstdint>
+
+typedef uint32_t u32;
+typedef uint8_t u8;
+
+
+
+
+/**
+ * Error codes for the GC9A01 display driver
+ */
+typedef enum gc9a01_err_t {
+    GC9A01_OK,
+} gc9a01_err_t;
+
+typedef struct color_t {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} color_t;
+
+typedef struct gc9a01_cmd_t {
+    u8 cmd;
+    u8 data[16];
+    u8 datasize;
+} gc9a01_cmd_t;
+
+
+class GC9A01 {
+public:
+    GC9A01();
+    ~GC9A01() = default;
+
+    // NOTE: Maybe arguments needed
+    gc9a01_err_t init();
+
+    gc9a01_err_t draw_pixel     (u32 x, u32 y, color_t color) const;
+    gc9a01_err_t draw_string    (u32 x, u32 y, const char* str) const;
+    gc9a01_err_t draw_box       (u32 x, u32 y, u32 w, u32 h, u32 thickness, color_t color) const;
+    gc9a01_err_t draw_circle    (u32 x, u32 y, u32 r, u32 thickness, color_t color) const;
+    gc9a01_err_t draw_line      (u32 x, u32 y, u32 r, u32 thickness, color_t color) const;
+
+    gc9a01_err_t draw_bitmap    (u32 x, u32 y, u32 w, u32 h, u8* buffer);
+
+    gc9a01_err_t fill_box       (u32 x, u32 y, u32 w, u32 h, color_t color) const;
+    gc9a01_err_t fill_circle    (u32 x, u32 y, u32 r, color_t color) const;
+
+    // NOTE: Arguments needed for font
+    gc9a01_err_t set_font       (void);
+
+private:
+};
